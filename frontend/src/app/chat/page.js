e11526435916fc13d47f18c174db913261fa9870
rudra100008 <ashumbhuju123@gmail.com>
2024-12-16 18:27:42 +0545

    Frontend of the chat app

--- frontend/src/app/chat/page.js
+++ frontend/src/app/chat/page.js
@@ -1,26 +1,36 @@
 "use client"
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import style from '../Style/chat.module.css'
-export default function Chat(){
-    useEffect(()=>{
-        document.title ="Chat"
-    },[])
-    return(
-        <div>
+export default function Chat() {
+    const [message, setMessage] = useState([])
+    const [inputValue,setInputValue] = useState('');
+    const handleValueChange=(e)=>{
+        setInputValue(e.target.value)
+    }
+    return (
+        <div className={style.body}>
             <div className={style.ChatContainer}>
-            <h1 className={style.headers}>Chat</h1>
-             <div className={style.FieldGroup}>
-             <input 
-             type="text"
-             name='content'
-             id='content'
-             placeholder='Type a message'
-             className={style.FieldInput}
-             />
-             </div>
+            <div>{inputValue}</div>
+                <div className={style.FieldGroup}>
+                    <input
+                        type="text"
+                        name='content'
+                        id='content'
+                        placeholder='Type a message'
+                        className={style.FieldInput}
+                        value={inputValue}
+                        onChange={handleValueChange}
+                        onKeyPress={(e)=> e.key === "Enter" && handleSendMessage()}
+                    />
+                </div>
+                <div className={style.ButtonGroup}>
+                    <button className={style.SendButton}>
+                        Send
+                    </button>
+                </div>
             </div>
         </div>
-        
-          
+
+
     )
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,12 @@
+"use client"
+import { useEffect } from 'react';
 import Chat from './chat/page';
 import style from './Style/home.module.css'
 
 export default function Home() {
+  useEffect(()=>{
+    document.title = "Chat App";
+  },[])
   return (
     <div >
       <Chat/>
