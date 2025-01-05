f886814efa4f0a57ce94c60eec85cafa5de534ca
rudra100008 <ashumbhuju123@gmail.com>
2025-01-05 17:47:04 +0545

    Frontend of the chat app

--- frontend/src/app/signup/page.js
+++ frontend/src/app/signup/page.js
@@ -5,6 +5,27 @@ export default function SignUp(){
                 <h1>Sign Up</h1>
                 <div className="">
                     <label htmlFor="username">Username</label>
+                    <input
+                    id="username"
+                    name="username"
+                    placeholder="Enter user name"
+                    />
+                </div>
+                <div className="">
+                    <label htmlFor="email">Email</label>
+                    <input
+                    id="email"
+                    name="email"
+                    placeholder="Enter email"
+                    />
+                </div>
+                <div className="">
+                    <label htmlFor="password">Password</label>
+                    <input
+                    id="password"
+                    name="password"
+                    placeholder="Enter password"
+                    />
                 </div>
             </form>
         </div>
