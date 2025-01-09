a24db2f4ab83d6abd6aef61007d0ff0a2970b4a5
rudra100008 <ashumbhuju123@gmail.com>
2025-01-09 19:16:05 +0545

    Backend of the chat app

--- Backend/src/main/java/com/ChatApplication/Repository/UserRepository.java
+++ Backend/src/main/java/com/ChatApplication/Repository/UserRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Repository
 @Transactional
 public interface UserRepository extends MongoRepository<User,String> {
      Boolean existsByEmail(String email);
      Boolean existsByUserName(String username);
      Boolean existsByPhoneNumber(String phoneNumber);
+     Optional<User> findByEmail(String email);
+     Optional<User> findByUserName(String username);
+     Optional<User> findByPhoneNumber(String phoneNumber);
 }
