c1d566b99377b27c4a624062d70e47e00c72250a
rudra100008 <ashumbhuju123@gmail.com>
2025-01-12 18:39:21 +0545

    backend of the chat app

--- Backend/src/main/java/com/ChatApplication/Security/ApplicationConfiguration.java
+++ Backend/src/main/java/com/ChatApplication/Security/ApplicationConfiguration.java
@@ -1,9 +1,44 @@
 package com.ChatApplication.Security;
 
+import com.ChatApplication.Exception.ResourceNotFoundException;
+import com.ChatApplication.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfiguration {
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByPhoneNumber(username)
+                .orElseThrow(()-> new ResourceNotFoundException(username + " not found"));
+    }
+    @Bean
+    public BCryptPasswordEncoder bcryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    //core component that process  authentication requests
+    // the authenticationManager receives the authenticationRequest
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+    // this loads the userDetails through the userDetailsService()
+    // this verify the password through the bcryptPasswordEncoder()
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(bcryptPasswordEncoder());
+        return authenticationProvider;
+    }
 }
