5c7ad50404a623e2e135c1ed8c846517a08d9e1a
rudra100008 <ashumbhuju123@gmail.com>
2025-01-13 18:41:20 +0545

    Backend of the chat app

---
+++ Backend/src/main/java/com/ChatApplication/Security/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.ChatApplication.Security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+}
