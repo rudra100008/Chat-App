5da138ceac48f1ceff262c4ab5375951b5ff4a0c
rudra100008 <ashumbhuju123@gmail.com>
2025-01-15 19:04:49 +0545

    Backend of chat app

--- Backend/src/main/java/com/ChatApplication/Security/JwtAuthenticationFilter.java
+++ Backend/src/main/java/com/ChatApplication/Security/JwtAuthenticationFilter.java
@@ -24,6 +24,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader =  request.getHeader("Authorization");
+        if(authHeader ==  null || !authHeader.contains("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        
         
     }
 }
