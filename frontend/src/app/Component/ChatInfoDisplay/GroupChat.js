c602604e27d07777c8a2d936ec5bcc2899dda173
rudra100008 <ashumbhuju123@gmail.com>
2025-06-19 19:57:12 +0545

    Frontend

--- frontend/src/app/Component/ChatInfoDisplay/GroupChat.js
+++ frontend/src/app/Component/ChatInfoDisplay/GroupChat.js
@@ -2,12 +2,48 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import style from "../../Style/chatInfoDisplay.module.css";
 import GetGroupImage from "../GetGroupImage";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 const GroupChat = ({ chatData }) => {
     const [showEditChat,setShowEditChat] = useState(false);
+    const [showEditChatName,setShowEditChatName] = useState(false);
+    const [localChatData,setLocalChatData] = useState(chatData);
+    const [inputWidth,setInputWidth] = useState(100);
+    const inputRef = useRef(null);
+    const spanRef = useRef(null);
+
     const handleEditChat = ()=>{
         setShowEditChat(prev=> !prev);
     }
+    const handleChatName = () => {
+        setShowEditChatName(prev => !prev);
+    }
+    const handleValueChange = (e) =>{
+        const {name,value} = e.target;
+        setLocalChatData(prev=>({
+            ...prev,
+            [name]:value
+        }))
+    }
+
+    useEffect(()=>{
+        const handleClickOutSide = (event) =>{
+            if(showEditChatName && inputRef.current && !inputRef.current.contains(event.target)){
+                setShowEditChatName(false);
+            }
+        }
+        document.addEventListener("mousedown",handleClickOutSide);
+        return ()=>{
+            setLocalChatData(chatData);
+            document.removeEventListener("mousedown",handleClickOutSide);
+        }
+    },[showEditChatName])
+
+    useEffect(()=>{
+        if(spanRef.current){
+            const width = spanRef.current.offsetWidth;
+            setInputWidth(Math.max(100,width+20));
+        }
+    },[localChatData.chatName])
     return (
         <div className={style.infoDisplayContainer}>
             <div className={style.faEdit}>
@@ -16,16 +52,35 @@ const GroupChat = ({ chatData }) => {
             <div className={style.image}>
                 <GetGroupImage chatId={chatData.chatId} size={120} />
             </div>
-            <p className={style.chatName}>{chatData.chatName}</p>
+            {
+                showEditChatName ? 
+                <div ref={inputRef}>
+                    <span ref={spanRef} className={style.hiddenSpan}>
+                        {localChatData.chatName || ""}
+                    </span>
+                <input 
+                type="text"
+                name="chatName"
+                id="chatName"
+                value={localChatData.chatName}
+                className={style.InputStyle}
+                onChange={handleValueChange}
+                style={{width:inputWidth}}
+                />
+                </div> :
+                <p className={style.chatName} onDoubleClick={handleChatName}>{chatData.chatName}</p>
+            }
             <div className={style.chatInfoDisplay}>
                 {/* <FontAwesomeIcon icon={}/> */}
+                <p>CreatedAt</p>
                 <p>{
                     new Date(chatData.createdAt).toLocaleDateString("en-us",{
                         day : "2-digit",
                         month : '2-digit',
                         year: "numeric"
                     }) || "unknown time"
-                    }</p>
+                    }
+                </p>
             </div>
         </div>
     )
